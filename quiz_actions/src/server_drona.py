e985916320684b2de816f0e2ebc514bafbdc8902
Cristian1077 <cristianganta10@gmail.com>
2026-05-18 15:42:12 +0300

    Add files via upload

---
+++ quiz_actions/src/server_drona.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy
+import actionlib
+from std_msgs.msg import Empty
+from quiz_actions.msg import DronaAction, DronaFeedback
+
+server = None
+pub_takeoff = None
+pub_land = None
+fb_msg = DronaFeedback()
+
+
+def execute(goal_msg):
+    global server
+    command = goal_msg.goal
+    r = rospy.Rate(1)
+    if command == "TAKEOFF":
+        pub_takeoff.publish(Empty())
+        fb_msg.feedback = "Decoleaza"
+        while not rospy.is_shutdown():
+            server.publish_feedback(fb_msg)
+            if server.is_preempt_requested():
+                server.set_preempted()
+                return
+            r.sleep()
+    elif command == "LAND":
+        pub_land.publish(Empty())
+        fb_msg.feedback = "Aterizeaza"
+        for i in range(3):
+            if server.is_preempt_requested():
+                server.set_preempted()
+                return
+            server.publish_feedback(fb_msg)
+            r.sleep()
+        server.set_succeeded(res_msg)
+
+if __name__ == "__main__":
+    rospy.init_node("Cristinei")
+    pub_takeoff = rospy.Publisher("/ardrone/takeoff", Empty, queue_size = 1)
+    pub_land = rospy.Publisher("/ardrone/land", Empty, queue_size = 1)
+    server = actionlib.SimpleActionServer("Drona", DronaAction, execute, False)
+    server.start()
+    rospy.spin()
